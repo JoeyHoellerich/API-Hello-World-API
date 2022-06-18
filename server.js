ade50b3e6a428d7030c734604c385ef9fd177e9a
Joey <joeyhoellerich@gmail.com>
2022-06-18 10:18:56 -0400

    added controller + model + seed data

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
     () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
 )
 
+// Languages: 
+const languagesController = require('./controllers/languages_controller.js')
+app.use('/languages', languagesController)
+
+
 // MIDDLEWARE
 app.use(express.urlencoded({extended: true}))
 
